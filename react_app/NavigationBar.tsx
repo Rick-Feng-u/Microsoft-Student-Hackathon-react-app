ed55bc5bdd310fd497e1e67543596fb752d7d8d9
Connor Doman <domanc@student.ubc.ca>
2021-10-16 16:43:38 -0700

    react-native-paper boilerplate, navbar

---
+++ react_app/NavigationBar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { BottomNavigation, Text } from 'react-native-paper';
+
+const HistoryRoute = () => {
+    return (
+        <Text>History</Text>
+    );
+};
+
+const CameraRoute = () => {
+    return (
+        <Text>Camera</Text>
+    );
+};
+
+const ImportRoute = () => {
+    return (
+        <Text>Camera</Text>
+    );
+};
+
+
+export const NavigationBar = () => {
+    const [index, setIndex] = React.useState(1);
+    const [routes] = React.useState([
+        { key: 'history', title: 'History', icon: 'book-open-variant' },
+        { key: 'camera', title: 'Camera', icon: 'barcode-scan' },
+        { key: 'import', title: 'Import', icon: 'card-text-outline' }
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+        history: HistoryRoute,
+        camera: CameraRoute,
+        import: ImportRoute
+    });
+
+    return (
+        <BottomNavigation
+            navigationState={{ index, routes }}
+            onIndexChange={setIndex}
+            renderScene={renderScene}
+        />
+    );
+};

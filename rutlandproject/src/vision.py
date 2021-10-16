e0ff3a9af7f343baaddd72397751c436c5dc954a
Sean Roarty <seanroarty@gmail.com>
2021-10-16 15:48:48 -0700

    Initialization.

---
+++ rutlandproject/src/vision.py
@@ -0,0 +1,13 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+import os
+region = os.environ['ACCOUNT_REGION']
+key = os.environ['ACCOUNT_KEY']
+
+credentials = CognitiveServicesCredentials(key)
+client = ComputerVisionClient(
+    endpoint="https://" + region + ".api.cognitive.microsoft.com/",
+    credentials=credentials
+)

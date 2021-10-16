e5085ecfb8fe21206f75ae9e3674e407a7783977
Sean Roarty <seanroarty@gmail.com>
2021-10-16 15:58:05 -0700

    Inserted boilerplate brand detection

--- rutlandproject/src/vision.py
+++ rutlandproject/src/vision.py
@@ -11,3 +11,24 @@ client = ComputerVisionClient(
     endpoint="https://" + region + ".api.cognitive.microsoft.com/",
     credentials=credentials
 )
+
+'''
+Detect Brands - remote
+This example detects common brands like logos and puts a bounding box around them.
+'''
+print("===== Detect Brands - remote =====")
+# Get a URL with a brand logo
+remote_image_url = "https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/images/gray-shirt-logo.jpg"
+# Select the visual feature(s) you want
+remote_image_features = ["brands"]
+# Call API with URL and features
+detect_brands_results_remote = client.analyze_image(remote_image_url, remote_image_features)
+
+print("Detecting brands in remote image: ")
+if len(detect_brands_results_remote.brands) == 0:
+    print("No brands detected.")
+else:
+    for brand in detect_brands_results_remote.brands:
+        print("'{}' brand detected with confidence {:.1f}% at location {}, {}, {}, {}".format( \
+        brand.name, brand.confidence * 100, brand.rectangle.x, brand.rectangle.x + brand.rectangle.w, \
+        brand.rectangle.y, brand.rectangle.y + brand.rectangle.h))

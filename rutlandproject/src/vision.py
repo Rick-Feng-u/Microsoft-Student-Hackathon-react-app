d47ab2128adce6b2e60d5685c59d93df9452d97a
Sean Roarty <seanroarty@gmail.com>
2021-10-16 23:27:05 -0700

    Added some test images, and changed vision.py to perform base64 decoding

--- rutlandproject/src/vision.py
+++ rutlandproject/src/vision.py
@@ -5,38 +5,54 @@ from dotenv import load_dotenv
 import requests
 from bing import bing_search
 from sentiment_analysis import extract_sentiment
+import base64
 
-load_dotenv()  # take environment variables from .env.
-import os
-region = os.environ['ACCOUNT_REGION']
-key = os.environ['VISION_KEY']
 
-credentials = CognitiveServicesCredentials(key)
-client = ComputerVisionClient(
-    endpoint="https://" + region + ".api.cognitive.microsoft.com/",
-    credentials=credentials
-)
 
-image_path = "../bin/crest.png"
-endpoint = "https://" + region + ".api.cognitive.microsoft.com/"
-analyze_url = endpoint + "vision/v3.1/analyze"
+def brand_analysis(image):
+    """
+    Takes a user provided image, extracts the brand, and performs
+    a sentiment analysis based on bing news headlines
+    
+    image object has the following fields:
+    b64string: base64 encoded string representation of image
+    extension: the image's original finle extension"
+    """
+    imgdata = base64.b64decode(image["b64string"])
+    filename = '../bin/photo.%s' % image["extension"]
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
 
-# Read the image into a byte array
-image_data = open(image_path, "rb").read()
-headers = {'Ocp-Apim-Subscription-Key': key,
-           'Content-Type': 'application/octet-stream'}
-params = {'visualFeatures': 'brands'}
-response = requests.post(
-    analyze_url, headers=headers, params=params, data=image_data)
-response.raise_for_status()
+    load_dotenv()  # take environment variables from .env.
+    import os
+    region = os.environ['ACCOUNT_REGION']
+    key = os.environ['VISION_KEY']
 
-# The 'analysis' object contains various fields that describe the image. The most
-# relevant caption for the image is obtained from the 'description' property.
-analysis = response.json()
-try:
-    print("Running search on ",analysis["brands"][0]["name"])
-    headlines = bing_search(analysis["brands"][0]["name"])
-    extract_sentiment(headlines)
+    credentials = CognitiveServicesCredentials(key)
+    client = ComputerVisionClient(
+        endpoint="https://" + region + ".api.cognitive.microsoft.com/",
+        credentials=credentials
+    )
 
-except e:
-    print(e)
+    endpoint = "https://" + region + ".api.cognitive.microsoft.com/"
+    analyze_url = endpoint + "vision/v3.1/analyze"
+
+    # Read the image into a byte array
+    image_data = open(filepath, "rb").read()
+    headers = {'Ocp-Apim-Subscription-Key': key,
+            'Content-Type': 'application/octet-stream'}
+    params = {'visualFeatures': 'brands'}
+    response = requests.post(
+        analyze_url, headers=headers, params=params, data=image_data)
+    response.raise_for_status()
+
+    # The 'analysis' object contains various fields that describe the image. The most
+    # relevant caption for the image is obtained from the 'description' property.
+    analysis = response.json()
+    try:
+        print("Running search on ",analysis["brands"][0]["name"])
+        headlines = bing_search(analysis["brands"][0]["name"])
+        extract_sentiment(headlines)
+
+    except e:
+        print(e)

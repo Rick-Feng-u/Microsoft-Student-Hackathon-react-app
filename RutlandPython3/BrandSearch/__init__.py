dadb81fc04343cd421951f192347c2286e6de3e7
Ivan Carvalho <ivancarv@student.ubc.ca>
2021-10-18 19:03:18 -0700

    Simplify response and add bing

--- RutlandPython3/BrandSearch/__init__.py
+++ RutlandPython3/BrandSearch/__init__.py
@@ -55,24 +55,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             photo = req_body.get('photo')
 
     RESPONSE = {
-        "photo": photo,
-        "logo": "B64ENCODEDSTRING",
-        "parent_company": "Parent company",
         "brand_name": "Brand name",
-        "overall_rating": 4.2,
-        "ethical_rating": 4.1,
-        "site_ratings":[
-            {
-                "site_name":"Site name",
-                "site_rating": 4.2
-            }
-        ],
-        "sources":[
-            {
-                "name":"NAMEOFSOURCE",
-                "url":"URLOFSOURCE"
-            }
-        ]
+        "overall_rating": 4,
+        "ethical_rating": 4,
     }
 
     if photo:
